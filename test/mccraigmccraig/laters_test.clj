3fc425569f864b0732744c787f62e7d590056779
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2021-02-22 09:06:53 +0000

    delete junk

--- test/mccraigmccraig/laters_test.clj
+++
@@ -1,7 +0,0 @@
-(ns mccraigmccraig.laters-test
-  (:require [clojure.test :refer :all]
-            [mccraigmccraig.laters :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
